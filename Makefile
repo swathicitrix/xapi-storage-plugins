3459c2149f06913e36d883e33c17fa508dc4cbc9
Stefano Panella <Stefano.Panella@citrix.com>
2016-02-24 09:39:43 +0000

    rename lib to common

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-LIB_FILES=__init__.py device.py iscsi.py losetup.py tapdisk.py dmsetup.py nbdclient.py nbdtool.py image.py libvhd.py poolhelper.py
+COMMON_FILES=__init__.py device.py iscsi.py losetup.py tapdisk.py dmsetup.py nbdclient.py nbdtool.py image.py libvhd.py poolhelper.py
 PLUGINS=suspend-resume-datapath gfs2setup
 
 .PHONY: clean
@@ -6,7 +6,7 @@ clean:
 
 DESTDIR?=/
 SCRIPTDIR?=/usr/libexec/xapi-storage-script
-PYTHONDIR?=/usr/lib/python2.7/site-packages/xapi/storage/lib
+PYTHONDIR?=/usr/lib/python2.7/site-packages/xapi/storage/common
 PLUGINSDIR?=/etc/xapi.d/plugins
 
 install:
@@ -29,6 +29,6 @@ install:
 	(cd $(DESTDIR)$(SCRIPTDIR)/volume/org.xen.xapi.storage.gfs2; for link in Volume.destroy Volume.set_description Volume.stat Volume.clone Volume.resize Volume.set_name Volume.unset Volume.create Volume.set Volume.snapshot; do ln -s volume.py $$link; done)
 	(cd $(DESTDIR)$(SCRIPTDIR)/volume/org.xen.xapi.storage.gfs2; for link in SR.destroy SR.stat SR.attach SR.detach SR.create SR.ls ; do ln -s sr.py $$link; done)
 	mkdir -p $(DESTDIR)$(PYTHONDIR)
-	(cd lib; install -m 0755 $(LIB_FILES) $(DESTDIR)$(PYTHONDIR)/)
+	(cd common; install -m 0755 $(COMMON_FILES) $(DESTDIR)$(PYTHONDIR)/)
 	mkdir -p $(DESTDIR)$(PLUGINSDIR)
 	(cd overlay/etc/xapi.d/plugins; install -m 0755 $(PLUGINS) $(DESTDIR)$(PLUGINSDIR)/)
---
+++ common/poolhelper.py
@@ -0,0 +1,52 @@
+import xapi.storage.api.volume
+from xapi.storage import log
+import XenAPI
+
+
+def get_online_host_refs(dbg, session):
+    # This function is borrowed from xapi-project/sm.git/util.py
+    online_hosts = []
+    hosts = session.xenapi.host.get_all_records()
+    for host_ref, host_rec in hosts.iteritems():
+        metrics_ref = host_rec["metrics"]
+        metrics_rec = session.xenapi.host_metrics.get_record(metrics_ref)
+        if metrics_rec["live"]:
+            online_hosts.append(host_ref)
+    return online_hosts
+
+
+def call_plugin_in_pool(dbg, plugin_name, plugin_function, args):
+    log.debug("%s: calling plugin '%s' function '%s' with args %s in pool" % (dbg, plugin_name, plugin_function, args))
+    session = XenAPI.xapi_local()
+    try:
+        session.xenapi.login_with_password('root', '')
+    except:
+        # ToDo: We ought to raise something else
+        raise
+    try:
+        for host_ref in get_online_host_refs(dbg, session):
+            log.debug("%s: calling plugin '%s' function '%s' with args %s on host %s" % (dbg, plugin_name, plugin_function, args, host_ref))
+            resulttext = session.xenapi.host.call_plugin(
+                host_ref,
+                plugin_name,
+                plugin_function,
+                args)
+            log.debug("%s: resulttext = %s" % (dbg, resulttext))
+            if resulttext != "True":
+                # ToDo: We ought to raise something else
+                raise xapi.storage.api.volume.Unimplemented(
+                    "Failed to get hostref %s to run %s(%s)" %
+                    (host_ref, plugin_name, plugin_function, args))
+    except:
+        # ToDo: We ought to raise something else
+        raise
+    finally:
+        session.xenapi.session.logout()
+
+
+def suspend_datapath_in_pool(dbg, path):
+    call_plugin_in_pool(dbg, "suspend-resume-datapath", "suspend_datapath", {'path': path})
+
+
+def resume_datapath_in_pool(dbg, path):
+    call_plugin_in_pool(dbg, "suspend-resume-datapath", "resume_datapath", {'path': path})
