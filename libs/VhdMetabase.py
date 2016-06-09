4d256db0d7945e2da4882c79bf0fbc1fc154d706
Mark Syms <mark.syms@citrix.com>
2016-06-09 12:49:28 +0000

    Add vdi query operations to DB

--- libs/VhdMetabase.py
+++ libs/VhdMetabase.py
@@ -1,22 +1,64 @@
 #!/usr/bin/env python
 
 import sqlite3
+from contextlib import contextmanager
+
+class VDI(object):
+    def __init__(self, row):
+        self.key = row['key']
+        self.name = row['name']
+        self.parent = row['parent']
+        self.description = row['description']
+        self.snap = row['snap']
+        self.uuid = row['uuid']
+        self.vsize = row['vsize']
 
 class VhdMetabase(object):
 
     def __init__(self, path):
-        self.path = path
-        self.connect()
+        self.__path = path
+        self.__connect()
 
-    def connect(self):
-        self.conn = sqlite3.connect(self.path)
+    def __connect(self):
+        self._conn = sqlite3.connect(self.__path)
+        self._conn.row_factory = sqlite3.Row
 
     def create(self):
-        with self.conn:
-            self.conn.execute("create table VDI(key integer primary key, snap int,"
+        with self._conn:
+            self._conn.execute("create table VDI(key integer primary key, snap int,"
                          "parent int, name text, description text, vsize text,"
                          "uuid text, active_on text, gc_status text, nonpersistent integer)")
             # TODO: define indexes, parent, uuid, (active_on?)
 
+    def insert_new_vdi(self, name, description, uuid, vsize):
+        return self.__insert_vdi(None, name, description, uuid, vsize)
+
+    def insert_child_vdi(self, parent, name, description, uuid, vsize):
+        return self.__insert_vdi(parent, name, description, uuid, vsize)
+
+    def __insert_vdi(self, parent, name, description, uuid, vsize):
+        res = self._conn.execute(
+            "insert into VDI(snap, parent, name, description, uuid, vsize)"
+            " values (:snap, :parent, :name, :description,:uuid, :vsize)",
+            {"snap": 0,
+             "parent": parent,
+             "name": name,
+             "description": description,
+             "uuid": uuid,
+             "vsize": str(vsize)})
+        return res.lastrowid
+
+    def get_vdi_by_id(self, key_id):
+        res = self._conn.execute("select * from VDI where rowid=:row", {"row" : int(key_id)})
+        row = res.fetchone()
+        if (row):
+            return VDI(row)
+        return None
+
+    @contextmanager
+    def write_context(self):
+        with self._conn:
+            yield
+
     def close(self):
-        self.conn.close()
+        self._conn.close()
--- test/libs/VhdMetabaseTest.py
+++ test/libs/VhdMetabaseTest.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 from xapi.storage.libs import VhdMetabase
 
 class StubVhdMetabase(VhdMetabase.VhdMetabase):
@@ -7,11 +8,17 @@ class StubVhdMetabase(VhdMetabase.VhdMetabase):
         VhdMetabase.VhdMetabase.__init__(self, ":memory:")
 
     def schema_check(self):
-        with self.conn:
-            tables = self.conn.execute("PRAGMA table_info('VDI')")
+        with self._conn:
+            tables = self._conn.execute("PRAGMA table_info('VDI')")
             for row in tables:
                 print row
 
+    def populate_test_set_1(self):
+        with self.write_context():
+            id = self.insert_new_vdi("VDI1", "First VDI", str(uuid.uuid4()), 10*1024)
+            id = self.insert_new_vdi("VDI2", "Second VDI", None, 10*1024)
+            id = self.insert_child_vdi(id, "Child1", "First Child VDI", str(uuid.uuid4()), 10*1024)
+            pass
 
 class VhdMetabaseTest(unittest.TestCase):
 
@@ -25,3 +32,19 @@ class VhdMetabaseTest(unittest.TestCase):
     def test_database_create(self):
         self.subject.schema_check()
     
+    def test_vdi_get_by_id_success(self):
+        self.subject.populate_test_set_1()
+        vdi = self.subject.get_vdi_by_id(1)
+
+        self.assertEquals(1, vdi.key)
+        self.assertEquals("VDI1", vdi.name)
+        self.assertEquals("First VDI", vdi.description)
+        self.assertEquals(None, vdi.parent)
+        self.assertEquals(0, vdi.snap)
+        self.assertEquals(str(10*1024), vdi.vsize)
+
+    def test_vdi_get_by_missing_id_failure(self):
+        self.subject.populate_test_set_1()
+        vdi = self.subject.get_vdi_by_id(1000)
+
+        self.assertEquals(None, vdi)
